7a8637bf58d487d8e34e7f6298d33f1aa2571c28
walteremoore <walterenriquemoore@gmail.com>
2021-12-09 21:58:44 -0500

    views con clases

--- nombre_proyecto/views.py
+++ nombre_proyecto/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from apps.productos.models import Producto
+# from django.shortcuts import render
+# from apps.productos.models import Producto
+from django.views.generic.base import TemplateView
 
+
+"""
+# Inicio basado en función
 def inicio(request):
     contex = {
         "productos": Producto.objects.all()
     }
-    return render(request, "inicio.html", contex)
+    return render(request, "inicio.html", contex) 
+"""
 
+class Inicio(TemplateView):
+    template_name = "inicio.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Inicio, self).get_context_data(**kwargs)
+        context["informacion"] = "Estoy pasando cosas" 
+        return context
+    
